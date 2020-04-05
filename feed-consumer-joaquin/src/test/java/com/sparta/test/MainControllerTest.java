7caee97b7e18a6a498107e1180fe6f1df2d359dd
juako <juako@NCC1701A>
2020-04-05 16:14:39 +0200

    Resuelto problema con los tests cuando se ejecutaban desde línea de comandos

--- feed-consumer-joaquin/src/test/java/com/sparta/test/MainControllerTest.java
+++ feed-consumer-joaquin/src/test/java/com/sparta/test/MainControllerTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sparta.SpartaApplication;
 import com.sparta.controller.MainController;
 import com.sparta.message.builders.LoadBatchBuilder;
 import com.sparta.message.objects.Record;
 import com.sparta.utils.Converters;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(classes={SpartaApplication.class})
 public class MainControllerTest {
 
 	static Logger log = LogManager.getLogger(MainController.class);
@@ -27,6 +29,33 @@ public class MainControllerTest {
 	@Test
 	void indexShouldWork() {
 		assertNotNull(this.mainController.index());
+	}
+	
+	@Test
+	void loadBatchBuilderShouldWork() {
+		int total = 1;
+		List<Record> listTestData = null;
+		List<Record> listMessage = null;
+		byte[] content;
+		LoadBatchBuilder builder;
+		
+		try {
+			listTestData = LoadBatchBuilder.constructTestData(total);
+			content = LoadBatchBuilder.constructMessage(listTestData);
+			builder = new LoadBatchBuilder(content);
+			listMessage = builder.construct();
+		}catch(Exception e) {
+  			log.error(e);
+		}
+		assertNotNull(listTestData);
+		assertNotNull(listMessage);
+		assertEquals(listTestData.get(0).getCity(), listMessage.get(0).getCity(), "city");
+		assertEquals(listTestData.get(0).getIndex(), listMessage.get(0).getIndex(), "index");
+		assertEquals(listTestData.get(0).getTimestamp(), listMessage.get(0).getTimestamp(), "timestamp");
+		assertEquals(listTestData.get(0).getNumberBytesSensorData(), listMessage.get(0).getNumberBytesSensorData(), "numberBytesSensor");
+		assertEquals(listTestData.get(0).getCrc32SensorsData(), listMessage.get(0).getCrc32SensorsData(), "crc2sensorsData");
+		assertEquals(listTestData.get(0).getSensors().get(0).getId(), listMessage.get(0).getSensors().get(0).getId(), "sensors.id");
+		assertEquals(listTestData.get(0).getSensors().get(0).getMeasure(), listMessage.get(0).getSensors().get(0).getMeasure(), "sensors.measure");
 	}
 	
 	@Test
